8c4fc5b01de5b7f8b31ec0b80b33fe3fafd92bd9
Anuradha-A-H <119663653+Anuradha-A-H@users.noreply.github.com>
2024-08-03 21:38:32 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,69 @@
+
+var num = 0;
+var listOfobj = [];
+let btnsubmit = document.getElementById("formbtn");
+function dltrow(e)
+    {
+        let numberId = 0;
+        let listchildNodes = e.parentElement.childNodes;
+        let lsinodes = listchildNodes[0].childNodes;
+        for(var nodes of lsinodes)
+        {
+            if(nodes.className == "id")
+            {
+                numberId = nodes.innerText;
+                break;
+            }
+        }
+        // console.log(e.parentElement);
+        let totalval = ""; let n =0;
+        for(var nodes = 0;nodes<listOfobj.length;nodes++)
+        {
+            if(listOfobj[nodes].num == numberId)
+            {
+                n = numberId;
+            }else{
+            totalval += "<div class='d-flex justify-content-start mt-2'><div class='col-6 border border-white d-flex justify-content-between p-2 rounded'><span class='id'>" + listOfobj[nodes].num + "</span><span class='name'>   Name: "+listOfobj[nodes].name+" </span><span class='profession'>Profession: "+listOfobj[nodes].profession+" </span><span>Age: "+listOfobj[nodes].age+" </span></div><button class='btn btn-Light rounded ml-4 ' id='deleteBtn' onclick='dltrow(this)'>Delete User</button></div>";
+            }
+        }
+        console.log(n)
+        listOfobj.splice(n-1, 1);
+        let listob = document.getElementById("listObj");
+        listob.innerHTML = totalval;
+        console.log(listOfobj);
+        if(listOfobj.length == 0)
+            {
+                document.getElementById("empnum1").value = "You have 0 Employees.";
+            }
+    }
+btnsubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let profession = document.getElementById("profession").value;
+    if (name == "" || age == "" || profession == "") {
+        let error = document.getElementById("error");
+        error.innerText = "Error: Please Make sure All the fields are filled before adding in an employee !";
+        error.style.color = "red";
+    } else {
+        error.innerText = "Employee Added!";
+        error.style.color = 'green';
+        num++;
+        let obj = { "num":num, "name":name, "age":age, "profession":profession };
+        listOfobj.push(obj);
+        let listob = document.getElementById("listObj");
+        let totalval = "";
+        // console.log(listOfobj);
+        for (var curr  of listOfobj) {
+            // console.log(curr.name);
+            totalval += "<div class='d-flex justify-content-start mt-2'><div class='col-6 border border-white d-flex justify-content-between p-2 rounded'><span class='id'>" + curr.num + "</span><span class='name'>.   Name: "+curr.name+" </span><span class='profession'>Profession: "+curr.profession+" </span><span>Age: "+curr.age+" </span></div><button class='btn btn-Light rounded ml-4 ' id='deleteBtn' onclick='dltrow(this)'>Delete User</button></div>";
+        }
+        listob.innerHTML = totalval;
+        document.getElementById("name").value = "";
+        document.getElementById("age").value = "";
+        document.getElementById("profession").value = "";
+         document.getElementById("empnum1").value="";
+    }
+    
+    
+});
